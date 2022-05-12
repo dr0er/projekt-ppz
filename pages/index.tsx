5fd5314b8338c2f2d06dd11bb557c76ea856ffa6
dr0er <ernestxxx123@gmail.com>
2022-05-12 14:48:29 +0200

    add multiple styles and background

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import { PostWidget, PostCard, Categories } from '../components'
 
 const posts = [
   { title: 'React Testing', excerpt: 'Learn React Testing' },
@@ -16,17 +16,14 @@ const Home: NextPage = () => {
       </Head>
       <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
         <div className="col-span-1 lg:col-span-8">
-          {posts.map((post, index) => (
-            <div>
-              {post.title}
-              {post.excerpt}
-            </div>
+          {posts.map((post) => (
+            <PostCard post={post} key={post.title} />
           ))}
         </div>
         <div className="col-span-1 lg:col-span-4">
           <div className="relative top-8 lg:sticky">
-            {/* <PostWidget />
-            <Categories /> */}
+            <PostWidget />
+            <Categories />
           </div>
         </div>
       </div>

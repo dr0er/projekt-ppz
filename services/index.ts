bec925b9fc72c8032bf9a33fdc4f982351f8cf2c
dr0er <ernestxxx123@gmail.com>
2022-06-07 21:29:54 +0200

    add recent posts section

--- services/index.ts
+++ services/index.ts
@@ -38,3 +38,42 @@ export const getPosts = async () => {
 
   return result.postsConnection.edges
 }
+
+export const getCategories = async () => {
+  const query = gql`
+    query GetGategories {
+      categories {
+        name
+        slug
+      }
+    }
+  `
+
+  const result = await request(graphqlAPI, query)
+
+  return result.categories
+}
+
+export const getSimilarPosts = async (categories: any, slug: any) => {
+  const query = gql`
+    query GetPostDetails($slug: String!, $categories: [String!]) {
+      posts(
+        where: {
+          slug_not: $slug
+          AND: { categories_some: { slug_in: $categories } }
+        }
+        last: 3
+      ) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `
+  const result = await request(graphqlAPI, query, { slug, categories })
+
+  return result.posts
+}

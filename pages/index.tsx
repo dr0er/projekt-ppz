8a3bea31b99a82c608f8efcc3f7af933f96be7d2
dr0er <ernestxxx123@gmail.com>
2022-06-16 11:45:11 +0200

    add PostDetail logic & visuals

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,19 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import { PostWidget, PostCard, Categories } from '../components'
+import { PostCard, Categories, PostWidget } from '../components'
 import { getPosts } from '../services'
 
-// const posts = [
-//   { title: 'React Testing', excerpt: 'Learn React Testing' },
-//   { title: 'React Testing2', excerpt: 'Learn React Testing2' },
-// ]
-
-const Home = ({ posts }) => {
+export default function Home({ posts }) {
   return (
     <div className="container mx-auto mb-8 px-10">
-      <Head>
-        <title>Projekt PPZ</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      {/* <FeaturedPosts /> */}
       <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
         <div className="col-span-1 lg:col-span-8">
-          {posts.map((post) => (
-            <PostCard post={post.node} key={post.title} />
+          {posts.map((post, index) => (
+            <PostCard key={index} post={post.node} />
           ))}
         </div>
         <div className="col-span-1 lg:col-span-4">
           <div className="relative top-8 lg:sticky">
-            <PostWidget />
+            {/* <PostWidget /> */}
             <Categories />
           </div>
         </div>
@@ -39,5 +29,3 @@ export async function getStaticProps() {
     props: { posts },
   }
 }
-
-export default Home

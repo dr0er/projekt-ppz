ed6f605a093d398b19a282c54dc2973a654f24cf
dr0er <ernestxxx123@gmail.com>
2022-06-16 13:55:27 +0200

    add Comments logic & visuals

--- services/index.ts
+++ services/index.ts
@@ -144,3 +144,19 @@ export const submitComment = async (obj: any) => {
 
   return result.json()
 }
+
+export const getComments = async (slug: any) => {
+  const query = gql`
+    query GetComments($slug: String!) {
+      comments(where: { post: { slug: $slug } }) {
+        name
+        createdAt
+        comment
+      }
+    }
+  `
+
+  const result = await request(graphqlAPI, query, { slug })
+
+  return result.comments
+}
